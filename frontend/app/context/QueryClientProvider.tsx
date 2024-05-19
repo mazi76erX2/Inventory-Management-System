6004e7f4455a9e23273e445b158e80d1ccf98789
Xolani <mazi76erx@gmail.com>
2024-05-19 20:08:47 +0000

    Removed unnecessary files

--- frontend/app/context/QueryClientProvider.tsx
+++
@@ -1,8 +0,0 @@
-import { QueryClient, QueryClientProvider as RQQueryClientProvider } from '@tanstack/react-query';
-import { ReactNode } from 'react';
-
-const queryClient = new QueryClient();
-
-export const QueryClientProvider = ({ children }: { children: ReactNode }) => (
-  <RQQueryClientProvider client={queryClient}>{children}</RQQueryClientProvider>
-);
